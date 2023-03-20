30b7c18feb17b8a82f35f39bd8412c44a91c8c2f
David Lavu <63914401+DavidLavu@users.noreply.github.com>
2023-03-20 21:10:14 +0000

    Update PortfolioProject_DataQualityChecks.sql

--- PortfolioProject_DataQualityChecks.sql
+++ PortfolioProject_DataQualityChecks.sql
@@ -1,6 +1,13 @@
 --3: Data Quality Checks
+--The query performs data quality checks on the "2022_TRIP_DATA" table to ensure that the data is valid and consistent.
+
+--The query  checks if all rides in the MEMBER_CASUAL field are booked by either members or casuals, checks the ranges of latitude and longitude to ensure that they are within valid ranges, and checks if ride IDs are unique.
+
+--The query  also checks for null values in the rows and investigates if the blank fields are either due to empty strings, null, or whitespace. It checks for null values in the start and end station IDs, start and end station names, and start and end latitude and longitude values.
+
 --All rides should be booked by either members or casuals
--- check if we only have two distinct values in MEMBER-CASUAL column
+--to check if we only have two distinct values in MEMBER-CASUAL column
+
 SELECT DISTINCT 
  MEMBER_CASUAL
 FROM 
@@ -18,6 +25,7 @@ FROM "2022_TRIP_DATA";
 
 
 --Check if ride ids are unique or not (if multiple rides have the same ride ID.) having count >1
+
 SELECT
  ride_id, COUNT(1)
 FROM
@@ -38,12 +46,14 @@ WHERE
 -- We want to investigate if the blank fields are either due to empty strings, or null , or whitespaces
 
 -- Below query is to counter fileds which only have whitespaces 
+
 SELECT COUNT (*) 
 FROM "2022_TRIP_DATA"
 WHERE TRIM(START_STATION_ID) IS NULL OR TRIM(START_STATION_NAME) IS NULL;
 
 -- Counting number of nulls
 -- Checking on start and end stations
+
 SELECT COUNT (*) 
 FROM "2022_TRIP_DATA"
 WHERE START_STATION_ID IS NULL OR START_STATION_NAME IS NULL;
